bd905f50384445d2587118582b9dac559d973e39
Andrii <andreiandreik19@gmail.com>
2026-03-14 16:37:47 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const buyButton = document.getElementById("buy-btn");
+
+buyButton.addEventListener("click", function () {
+  window.location.href = "https://andrii-kuleshov.github.io/checkout-page/";
+});
